9e26acf03f1c6c58a8d13c041d26423f5e013467
JorgeFragaCal <jorge.fraga1997@gmail.com>
2020-02-17 22:01:52 +0100

    menu mobile

--- my-app/Front/src/components/Menu.js
+++ my-app/Front/src/components/Menu.js
@@ -1,5 +1,5 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
+import styled, { css } from "styled-components";
 import { Button } from "./Button";
 
 export const Wrapper = styled.section`
@@ -13,14 +13,68 @@ export const Wrapper = styled.section`
   text-align: center;
   background-color: ${props => props.theme.colors.dark};
   height: 5rem;
-  @media (max-width: ${props => props.theme.breackpoints.xl}) {
-    display: none;
+  @media (max-width: ${props => props.theme.breackpoints.lg}) {
+    position: fixed;
+    grid-template-columns: 1fr;
+    right: 10px;
+    padding: 1rem;
+    bottom: 9rem;
+    z-index: 100;
+    border-radius: 10px;
+    max-width: 200px;
+    text-align: center;
+    height: auto;
+    background-color: ${props => props.theme.colors.dark};
+    ${props =>
+      props.hidden &&
+      css`
+        display: none;
+      `};
   }
 `;
 export const NavbarItem = styled.li`
   font-size: ${props => props.theme.fonts.size_xs};
   list-style: none;
   box-shadow: 0px 30px 30px 20px ${props => props.theme.colors.dark};
+  @media (max-width: ${props => props.theme.breackpoints.lg}) {
+    box-shadow: none;
+  }
+`;
+export const MenuButton = styled.button`
+  background: ${props => props.theme.colors.secondary};
+  box-shadow: 0 5px 10px 0 rgba(0, 0, 0, 0.5);
+  position: fixed;
+  bottom: 4rem;
+  right: 1.5rem;
+  width: 64px;
+  height: 64px;
+  padding: 0;
+  margin: 0;
+  border-radius: 50%;
+  border: none;
+  color: ${props => props.theme.colors.light};
+  z-index: 100;
+  display: none;
+  @media (max-width: ${props => props.theme.breackpoints.lg}) {
+    display: block;
+  }
+`;
+
+export const SiteOverlay = styled.div`
+  opacity: 0.75;
+  position: fixed;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  margin: 0;
+  background: black;
+  z-index: 10;
+  display: block;
+  ${props =>
+    props.hidden &&
+    css`
+      display: none;
+    `};
 `;
 
 export const NavbarLink = styled.a`
@@ -32,30 +86,37 @@ export const NavbarLink = styled.a`
   }
 `;
 function Menu() {
+  const [open, setOpen] = useState(true);
   return (
-    <Wrapper>
-      <NavbarItem>
-        <NavbarLink>Logo</NavbarLink>
-      </NavbarItem>
-      <NavbarItem>
-        <NavbarLink href="#home">HOME</NavbarLink>
-      </NavbarItem>
-      <NavbarItem>
-        <NavbarLink href="#about">SOBRE MI</NavbarLink>
-      </NavbarItem>
-      <NavbarItem>
-        <NavbarLink href="#expertise">MI EXPERIENCIA</NavbarLink>
-      </NavbarItem>
-      <NavbarItem>
-        <NavbarLink href="#benefits">BENEFICIOS</NavbarLink>
-      </NavbarItem>
-      <NavbarItem>
-        <NavbarLink href="#contact">CONTACTO</NavbarLink>
-      </NavbarItem>
-      <NavbarItem>
-        <Button>INICIAR SESION</Button>
-      </NavbarItem>
-    </Wrapper>
+    <React.Fragment>
+      <Wrapper hidden={open ? true : false}>
+        <NavbarItem>
+          <NavbarLink>Logo</NavbarLink>
+        </NavbarItem>
+        <NavbarItem>
+          <NavbarLink href="#home">HOME</NavbarLink>
+        </NavbarItem>
+        <NavbarItem>
+          <NavbarLink href="#about">SOBRE MI</NavbarLink>
+        </NavbarItem>
+        <NavbarItem>
+          <NavbarLink href="#expertise">MI EXPERIENCIA</NavbarLink>
+        </NavbarItem>
+        <NavbarItem>
+          <NavbarLink href="#benefits">BENEFICIOS</NavbarLink>
+        </NavbarItem>
+        <NavbarItem>
+          <NavbarLink href="#contact">CONTACTO</NavbarLink>
+        </NavbarItem>
+        <NavbarItem>
+          <Button>INICIAR SESION</Button>
+        </NavbarItem>
+      </Wrapper>
+      <MenuButton onClick={e => setOpen(!open)}>
+        <i className="fa fa-bars fa-lg"></i>
+      </MenuButton>
+      <SiteOverlay hidden={open ? true : false} />
+    </React.Fragment>
   );
 }
 export { Menu };
